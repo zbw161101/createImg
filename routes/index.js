1a9d5f1c233f732b53b563298d7afc29924dd084
赵博文 <nick.bw.zhao@outlook.com>
2017-06-08 15:13:26 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const handleImg = require('../lib/handleImg');
+const path = require('path');
+    /* GET home page. */
+router.get('/:size/:bgColor/:fontColor', function(req, res, next) {
+
+    let conf = {
+        size: req.params.size,
+        bgColor: req.params.bgColor,
+        fontColor: req.params.fontColor
+    };
+
+    const imgName = path.join(__dirname, '../i/' + req.url.split('/').join('m') + '.jpg');
+
+    // res.sendFile(imgName, function(err) {
+        // if (err) {
+            
+            handleImg(conf, imgName, function() {
+                res.sendFile(imgName);
+            });
+        // }
+    // })
+});
+
+module.exports = router;
