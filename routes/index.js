81c62df1427e1f942850358ea99fd6452b058204
赵博文 <nick.bw.zhao@outlook.com>
2017-06-09 18:49:33 +0800

    添加注释

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,19 @@ const express = require('express');
 const router = express.Router();
 const handleImg = require('../lib/handleImg');
 const path = require('path');
-    /* GET home page. */
-router.get('/:size/:bgColor/:fontColor', function(req, res, next) {
+/* GET home page. */
+router.get('/:size/:bgColor/:fontColor', function (req, res, next) {
 
-    let conf = {
-        size: req.params.size,
-        bgColor: req.params.bgColor,
-        fontColor: req.params.fontColor
-    };
+  let conf = {
+    size: req.params.size,
+    bgColor: req.params.bgColor,
+    fontColor: req.params.fontColor
+  };
 
-    const imgName = path.join(__dirname, '../i/' + req.url.split('/').join('m') + '.jpg');
-
-    // res.sendFile(imgName, function(err) {
-        // if (err) {
-            
-            handleImg(conf, imgName, function() {
-                res.sendFile(imgName);
-            });
-        // }
-    // })
+  const imgName = path.join(__dirname, '../i/' + req.url.split('/').join('m') + '.jpg');
+  handleImg(conf, imgName, function () {
+    res.sendFile(imgName);
+  });
 });
 
 module.exports = router;
